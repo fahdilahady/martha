910041befd4799f6490606fa7facefb4492092e5
Fahdil Ahady Hazain <hazain.ahady@gmail.com>
2024-08-29 12:18:20 +0700

    setup lint

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl:"https://www.saucedemo.com/",
-    
+    baseUrl: 'https://www.saucedemo.com/',
+
     watchForFileChanges: false,
-    
 
     retries: {
       runMode: 0,
       openMode: 0,
     },
     setupNodeEvents(on, config) {
-     
+
     },
   },
 
